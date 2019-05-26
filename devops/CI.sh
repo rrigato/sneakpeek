aede0b39e01767e11e4936f5dc4aa602dab6d5e8
Ryan <rrigato@horizon.csueastbay.edu>
2019-05-26 08:40:48 -0500

    Added script to be used on the first initialization of the development environment

---
+++ devops/CI.sh
@@ -0,0 +1,6 @@
+################################
+#Used to create development repository
+#Unit tests are run on each commit
+#
+#This script only needs to be run once
+###############################
