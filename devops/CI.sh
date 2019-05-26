e4389da3a9bae98ff909ba953222a1701e45dfb2
Ryan <rrigato@horizon.csueastbay.edu>
2019-05-26 10:02:19 -0500

    Added CodeCommit Creation

--- devops/CI.sh
+++ devops/CI.sh
@@ -4,3 +4,7 @@
 #
 #This script only needs to be run once
 ###############################
+
+#note that the cli user needs to have https for username/password setup
+#or ssh public key uploaded to codecommit
+aws codecommit create-repository --repository-name sneakpeek
