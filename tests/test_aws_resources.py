7d39c8b33fc93bd99a1e5d489a0e8fd7dcdc1394
Ryan <rrigato@horizon.csueastbay.edu>
2019-05-27 13:27:00 -0500

    Updating python test script

--- tests/test_aws_resources.py
+++ tests/test_aws_resources.py
@@ -1,7 +1,31 @@
 import unittest
 import pandas as pd
+import requests
 
+
+class WebappLive(unittest):
+    '''Tests that the aws resources necessary for the webpage are running
+
+        Parameters
+        ----------
+        Returns
+        -------
+        Raises
+        ------
+    '''
+    def test_home_page():
+    '''Tests that the aws resources necessary for the webpage are running
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    '''
 print("Hello World - First Unit Test")
 if __name__ == '__main__':
+    #unittest.main(exit=False)
     print ("Hello World 2")
-    #unittest.main()
