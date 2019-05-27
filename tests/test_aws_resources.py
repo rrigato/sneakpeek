d39f8ed06605d791e7b1516ae226d7300b8637c7
Ryan <rrigato@horizon.csueastbay.edu>
2019-05-27 13:40:39 -0500

    Added a test that makes a get request to the website

--- tests/test_aws_resources.py
+++ tests/test_aws_resources.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 
-class WebappLive(unittest):
+class WebappLive(unittest.TestCase):
     '''Tests that the aws resources necessary for the webpage are running
 
         Parameters
@@ -13,19 +13,25 @@ class WebappLive(unittest):
         Raises
         ------
     '''
-    def test_home_page():
-    '''Tests that the aws resources necessary for the webpage are running
+    def test_home_page(self):
+        '''Tests that the aws resources necessary for the webpage are running
 
-        Parameters
-        ----------
+            Parameters
+            ----------
 
-        Returns
-        -------
+            Returns
+            -------
 
-        Raises
-        ------
-    '''
+            Raises
+            ------
+        '''
+        r = requests.get(
+            'http://dev-sneekpeek.s3-website-us-east-1.amazonaws.com/'
+        )
+        self.assertEqual(r.status_code, 200)
+        logging.info("The website is live")
+        import pdb; pdb.set_trace()
 print("Hello World - First Unit Test")
 if __name__ == '__main__':
-    #unittest.main(exit=False)
+    unittest.main(exit=False)
     print ("Hello World 2")
