616834d6d83df9fd8daad1d5754ebfa1eacc2d03
Ryan <rrigato@horizon.csueastbay.edu>
2019-06-15 12:38:11 -0500

    Updated the path we are pointing to

--- static/js/config.js
+++ static/js/config.js
@@ -1,8 +1,8 @@
 /***************
 Loads a json from the cognito_config.json file
-and assigns that json to the window._config variable 
+and assigns that json to the window._config variable
 *****************/
-$.getJSON("test.json",
-    function("cognito_config.json") {
-        window._config = json
+$.getJSON("js/cognito_config.json",
+    function(json_config) {
+        window._config = json_config
 });
