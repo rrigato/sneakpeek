d5e827e26fd4633eb75da9b6cb3102dcc3127015
Ryan <rrigato@horizon.csueastbay.edu>
2019-06-15 12:36:28 -0500

    Updated the JSON file parse logic

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,8 @@
-window._config = JSON.parse("cognito_config.json");
+/***************
+Loads a json from the cognito_config.json file
+and assigns that json to the window._config variable 
+*****************/
+$.getJSON("test.json",
+    function("cognito_config.json") {
+        window._config = json
+});
