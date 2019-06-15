ce3fff74ecbd4bd45c3ec89a2236e71fb08fac8d
Ryan <rrigato@horizon.csueastbay.edu>
2019-06-15 16:42:46 -0500

    referencing the full hostname when pulling json config file

--- static/js/config.js
+++ static/js/config.js
@@ -1,9 +1,10 @@
-console.log("hello world 1");
+//<script type="text/javascript" src="js/cognito_config.json"></script>
 /***************
 Loads a json from the cognito_config.json file
 and assigns that json to the window._config variable
 *****************/
-$.getJSON("js/cognito_config.json",
+
+$.getJSON(window.location.hostname + "/js/cognito_config.json",
     function(json_config) {
         window._config = json_config
 });
