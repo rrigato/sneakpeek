5d67bb360b0b71bae19285ccec9936de6496672f
Ryan <rrigato@horizon.csueastbay.edu>
2019-06-15 13:10:46 -0500

    trying to get the cognito config to load

--- static/js/config.js
+++ static/js/config.js
@@ -1,3 +1,4 @@
+console.log("hello world 1");
 /***************
 Loads a json from the cognito_config.json file
 and assigns that json to the window._config variable
@@ -6,3 +7,5 @@ $.getJSON("js/cognito_config.json",
     function(json_config) {
         window._config = json_config
 });
+
+console.log("hello world 2");
