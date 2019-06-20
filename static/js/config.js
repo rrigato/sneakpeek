cb07ed3e709bbedf461a83f8304e5997a190f7bd
Ryan <rrigato@horizon.csueastbay.edu>
2019-06-19 19:05:30 -0500

    Added documentation, removed fluff

--- static/js/config.js
+++ static/js/config.js
@@ -1,41 +1,23 @@
-//<script type="text/javascript" src="js/cognito_config.json"></script>
-/***************
-Loads a json from the cognito_config.json file
-and assigns that json to the window._config variable
-*****************
-
-var x = $.getJSON("/js/cognito_config.json",
-    function(json_config) {
-        return(json_config);
-
-});
 
 
-
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-1_uWSFSS2Ph', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '16vk70001evfl5bhs21g6knf63', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
-*/
-
-
+/**********************
+*This ensures that the getJSON is a synchronous
+*call, makes sure that nothing else is done
+*until the json is loaded
+************************/
 $.ajaxSetup({
     async: false
 });
 
-
+/********************************
+*Loads the json file and assigns it
+*to the _config variable of window object
+*That way anytime config.js is loaded
+*the cognito user pool information in addition to
+*the api url will be available
+*********************************/
 $.getJSON("js/cognito_config.json",
     function(json_config) {
-        console.log(json_config);
         window._config = json_config;
 
 });
-
-
-console.log("hello world 2");
