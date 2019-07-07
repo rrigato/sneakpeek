884e1dc07cdb7a830fd3b75b4086f58306527138
Ryan <rrigato@horizon.csueastbay.edu>
2019-07-07 16:28:11 -0500

    Updated the image model

--- models/image_model.py
+++ models/image_model.py
@@ -1,6 +1,13 @@
+from sagemaker import get_execution_role
+from sagemaker.amazon.amazon_estimator import get_image_uri
+
 import boto3
 import logging
 import os
+
+
+
+
 
 class utilization:
     '''Image Classification Model for trailer utilization
@@ -32,6 +39,48 @@ class utilization:
              datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG
              )
         logging.info('\n')
+
+    def setup_notebook(self, s3_bucket):
+        '''Sets up jupyter notebook instance
+
+            Parameters
+            ----------
+            s3_bucket : str
+                Name of s3 bucket where images are stored in
+                Apache MXNET recordIO format
+
+            Returns
+            -------
+
+            Raises
+            ------
+        '''
+        role = get_execution_role()
+
+        bucket=s3_bucket
+
+        training_image = get_image_uri(
+            boto3.Session().region_name, 'image-classification'
+            )
+
+train_file = 'balanced_by_class_20_256_size.rec'
+val_file = 'all_images_256_size.rec'
+# The algorithm supports multiple network depth (number of layers). They are 18, 34, 50, 101, 152 and 200
+# For this training, we will use 18 layers
+num_layers = "18"
+# we need to specify the input image shape for the training data
+image_shape = "3,224,224"
+# we also need to specify the number of training samples in the training set
+# for caltech it is 15420
+num_training_samples = "80"
+# specify the number of output classes
+num_classes = "4"
+# batch size for training
+mini_batch_size =  "64"
+# number of epochs
+epochs = "2"
+# learning rate
+learning_rate = "0.01"
 
 def main():
     '''
