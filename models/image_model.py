ccc062ecc85386c822118d6629d5d10a271b6b0c
Ryan <rrigato@horizon.csueastbay.edu>
2019-07-07 15:36:49 -0500

    Successfully able to create a logging class

--- models/image_model.py
+++ models/image_model.py
@@ -1,14 +1,40 @@
 import boto3
+import logging
+import os
 
 class utilization:
     '''Image Classification Model for trailer utilization
     '''
-    def __init__:
+    def __init__(self):
+        '''Initializes the trailer utilization model
         '''
-        '''
+        self.get_logger()
 
-def get_logger(self):
-    '''Returns a logging instance for the script
+    def get_logger(self):
+        '''Returns a logging instance for the script
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        '''
+        """
+            Adds the file name to the logs/ directory without
+            the extension
+        """
+        logging.basicConfig(
+            filename=os.path.join(".", 'logs/',
+            os.path.basename(__file__).split('.')[0]),
+            format='%(asctime)s %(message)s',
+             datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG
+             )
+        logging.info('\n')
+
+def main():
+    '''
         Parameters
         ----------
 
@@ -18,14 +44,7 @@ def get_logger(self):
         Raises
         ------
     '''
-    """
-        Adds the file name to the logs/ directory without
-        the extension
-    """
-    logging.basicConfig(
-        filename=os.path.join(WORKING_DIRECTORY, 'logs/',
-        os.path.basename(__file__).split('.')[0]),
-        format='%(asctime)s %(message)s',
-         datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG
-         )
-    logging.info('\n')
+    model_build = utilization()
+
+if __name__ == "__main__":
+    main()
