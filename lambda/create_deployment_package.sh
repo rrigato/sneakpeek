6a696ebc292d9373c4aca7d944c2ad5b179b7305
Ryan <rrigato@horizon.csueastbay.edu>
2019-07-21 13:11:21 -0500

    Updated deployment pakcage shell script to exclude extraneous directories

--- lambda/create_deployment_package.sh
+++ lambda/create_deployment_package.sh
@@ -1,7 +1,12 @@
 ###############################
 #ride lambda function deployment package
+#--target installing the python dependency locally
+#to deploy for lambda
 #
-#
+#-9 = full size encryption
+#-j ignore directories between current working directory
+# and where the files are stores
+#(dont include lambda/ride in the zip)
 ###############################
-#pip install --target petl
-zip -9 ride.zip lambda/ride/*
+pip install --target . requests
+zip -9 -j ride.zip lambda/ride/*
