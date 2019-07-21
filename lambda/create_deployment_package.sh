5fb17909f9739fcb4398a2e064a803699d6fcead
Ryan <rrigato@horizon.csueastbay.edu>
2019-07-21 13:52:54 -0500

    Added logic for where the file is zipped

--- lambda/create_deployment_package.sh
+++ lambda/create_deployment_package.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+#gets current directory, concatenates with lambda/ride
+#cds there
+ORIGINAL_DIR=$(pwd)
+cd "$ORIGINAL_DIR/lambda/ride"
+
 ###############################
 #ride lambda function deployment package
 #--target installing the python dependency locally
@@ -8,5 +15,8 @@
 # and where the files are stores
 #(dont include lambda/ride in the zip)
 ###############################
+
 pip install --target ./lambda/ride requests
-zip -9j ride.zip lambda/ride/*
+zip -9rj ride.zip lambda/ride/*
+mv ride.zip $ORIGINAL_DIR
+cd $ORIGINAL_DIR
