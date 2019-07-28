75ac14f1b97f60afad7d00f8e9dcdeb109f89575
Ryan <rrigato@horizon.csueastbay.edu>
2019-07-28 13:56:36 -0500

    Adding a waiter for cloudformation stacks

---
+++ tests/cf_waiter.py
@@ -0,0 +1,24 @@
+"""
+The purpose of this script is to wait until the
+The cloudformation stack is created and complete
+"""
+import boto3
+def get_boto_clients(resource_name, region_name='us-east-1'):
+    '''Returns the boto client for various cloudformation resources
+        Parameters
+        ----------
+        resource_name : str
+            Name of the resource for the client
+
+        region_name : str
+                aws region you are using, defaults to
+                us-east-1
+
+        Returns
+        -------
+
+
+        Raises
+        ------
+    '''
+    return(boto3.client(resource_name, region_name))
