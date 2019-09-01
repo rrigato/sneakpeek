b36985a6842243c50fb1e0703faa432e106bca0c
Ryan <ryanrigato@gmail.com>
2019-09-01 18:42:30 -0500

    Working on identity provider information

--- static/js/upload.js
+++ static/js/upload.js
@@ -6,5 +6,21 @@
         ClientId: _config.cognito.userPoolClientId
     };
     var userPool = new AmazonCognitoIdentity.CognitoUserPool(cognitoUserPool);
+var cognitoUser = userPool.getCurrentUser();
+
+if (cognitoUser != null) {
+	cognitoUser.getSession(function(err, result) {
+		if (result) {
+			console.log('You are now logged in.');
+
+			// Add the User's Id Token to the
+            // Cognito identity pool credentials config session
+            AWS.config.region = 'us-east-1'; // Region
+            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                IdentityPoolId: 'us-east-1:5b715989-3e44-4988-87ef-46a625c974b5',
+            });
+		}
+	});
 }
+
 }(jQuery));
