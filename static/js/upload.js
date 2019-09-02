6aa4fcb083705580adde39f754bcb5ece3c4a8b8
Ryan <ryanrigato@gmail.com>
2019-09-01 19:35:59 -0500

    Added a variable to construct the user pool sign in string

--- static/js/upload.js
+++ static/js/upload.js
@@ -11,14 +11,24 @@ var cognitoUser = userPool.getCurrentUser();
 if (cognitoUser != null) {
 	cognitoUser.getSession(function(err, result) {
 		if (result) {
+
+            //Constructing user pool identity provider sign
+            //in string
+            var userPoolSignIn =  (
+            'cognito-idp.' + _config.cognito.region
+            + '.amazonaws.com/'
+            + _config.cognito.userPoolId
+            );
 			console.log('You are now logged in.');
 
 			// Add the User's Id Token to the
             // Cognito identity pool credentials config session
-            AWS.config.region = 'us-east-1'; // Region
             AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                IdentityPoolId: 'us-east-1:5b715989-3e44-4988-87ef-46a625c974b5',
-            });
+				IdentityPoolId: 'us-east-1:5b715989-3e44-4988-87ef-46a625c974b5',
+				Logins: {
+					: result.getIdToken().getJwtToken()
+				}
+			});
 		}
 	});
 }
