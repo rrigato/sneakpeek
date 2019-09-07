74054da1ffb81ef374bcfbe4d2ff782740af83f6
Ryan <ryanrigato@gmail.com>
2019-09-06 20:57:22 -0500

    Added an ajax call to return the json

--- static/js/results.js
+++ static/js/results.js
@@ -24,10 +24,31 @@ $(document).ready(function(){
 
 
 
+    function appendTable(){
+        /*********
+        *Appends html table with json response
+        *
+        **********/
+        $.getJSON( "http://dev-sneakpeek.s3-website-us-east-1.amazonaws.com/js/test_results.json",
+        function( json_response ) {
+            console.log(json_response);
+        //   var items = [];
+        //   $.each( data.data, function( key, val ) {
+        //     items.push( "<li id='" + key + "'>" + val + "</li>" );
+        //   });
+         /**
+          $( "<ul/>", {
+            "class": "my-new-list",
+            html: items.join( "" )
+          }).appendTo( "body" );
+          */
+        });
 
+    }
 
 
     verifySignIn();
+
 
 
 });
