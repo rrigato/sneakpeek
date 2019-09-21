531c067ccbc5e99210bd928f6a15218cb5bde5ab
Ryan <ryanrigato@gmail.com>
2019-09-21 12:43:13 -0500

    Adding logic and documentation for capturing user input

--- static/js/upload.js
+++ static/js/upload.js
@@ -251,9 +251,16 @@ function createPool(){
    });
 
 
+
+   /**
+   *overlay logic: inspired from this codepen:
+   *https://codepen.io/rkpasia/pen/LNEQod
+   *records the load id value
+   */
    $('.email').on("change keyup paste",
      function(){
        if($(this).val()){
+
          $('.icon-paper-plane').addClass("next");
        } else {
          $('.icon-paper-plane').removeClass("next");
@@ -269,53 +276,14 @@ function createPool(){
 
    $('.next-button.email').click(
      function(){
-       console.log("Something");
+
+        console.log($(this).val());
        $('.email-section').addClass("fold-up");
        $('.password-section').removeClass("folded");
      }
    );
 
-   $('.password').on("change keyup paste",
-     function(){
-       if($(this).val()){
-         $('.icon-lock').addClass("next");
-       } else {
-         $('.icon-lock').removeClass("next");
-       }
-     }
-   );
 
-   $('.next-button').hover(
-     function(){
-       $(this).css('cursor', 'pointer');
-     }
-   );
-
-   $('.next-button.password').click(
-     function(){
-       console.log("Something");
-       $('.password-section').addClass("fold-up");
-       $('.repeat-password-section').removeClass("folded");
-     }
-   );
-
-   $('.repeat-password').on("change keyup paste",
-     function(){
-       if($(this).val()){
-         $('.icon-repeat-lock').addClass("next");
-       } else {
-         $('.icon-repeat-lock').removeClass("next");
-       }
-     }
-   );
-
-   $('.next-button.repeat-password').click(
-     function(){
-       console.log("Something");
-       $('.repeat-password-section').addClass("fold-up");
-       $('.success').css("marginTop", 0);
-     }
-   );
 
 
 });
